2c9ff2fde86b6abaf50b1b0637a9c7ba6ab0f9a0
hoangnguyen-cloud84 <hoangnguyen-cloud84@gmail.com>
2024-05-28 16:49:02 +0700

    fix: pages

--- pages/blog.js
+++ pages/blog.js
@@ -4,8 +4,7 @@ import styles from '../styles/Blog.module.css';
 export default function Blog() {
   return (
     <div className={styles.container}>
-      <h1>DevOps Roadmap 2024</h1>
-      <Image src="/devops-roadmap.png" alt="DevOps Roadmap 2024" width={1024} height={2047} />
+        <Image src="/devops-roadmap.png" alt="DevOps Roadmap 2024" width={1128} height={3581} />
     </div>
   );
 }
